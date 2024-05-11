2338d8aa439d2670187534d846027d30551421d7
Богдан Козороз <sir.bodik2011@outlook.com>
2024-05-11 13:32:50 +0300

    Server created, sockets added, last change - playersConnected, project status: WIP

---
+++ public/script.js
@@ -0,0 +1,45 @@
+let roomUniqueId = null;
+console.log('script.js loaded');
+const socket = io();
+
+function createGame() {
+    socket.emit("createGame");
+  }
+
+  function joinGame() {
+    roomUniqueId = document.getElementById('roomUniqueId').value;
+    socket.emit('joinGame', {roomUniqueId: roomUniqueId});
+}
+
+
+socket.on("newGame", (data) => {
+    console.log("newGame");
+    roomUniqueId = data.roomUniqueId;
+    document.getElementById("initial").style.display = "none";
+    document.getElementById("gamePlay").style.display = "block";
+    let copyButton = document.createElement("button");
+    copyButton.style.display = "block";
+    copyButton.innerText = "Copy Code";
+    copyButton.addEventListener("click", () => {
+      navigator.clipboard.writeText(roomUniqueId).then(
+        function () {
+          console.log("Async: Copying to clipboard was successful!");
+        },
+        function (err) {
+          console.error("Async: Could not copy text: ", err);
+        }
+      );
+    });
+    document.getElementById(
+      "waitingArea"
+    ).innerHTML = `Waiting for opponent, please share code ${roomUniqueId} to join`;
+    document.getElementById("waitingArea").appendChild(copyButton);
+  });
+  
+  socket.on("playersConnected", (data) => {
+    document.getElementById("waitingArea").style.display = "none";
+    document.getElementById('gameArea').style.display = 'block';
+  });
+  
+  
+  
--- server/index.js
+++ server/index.js
@@ -1,8 +1,58 @@
-let http = require("http");
-const PORT = "8080";
+const express = require("express");
+const app = express();
+const http = require('http');
+const PORT = "3000";
+const path = require('path');
+const { Server } = require("socket.io");
+const rooms = {};
 
-http.createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.end("Hello server!");
-}).listen(PORT);
-console.log(`server listening on http://127.0.0.1:${PORT}`)
+const server = http.createServer(app);
+const io = new Server(server);
+
+io.on("connection", (socket) => {
+    console.log("a user is connected");
+    socket.on("disconnect", () => {
+      console.log("user disconnected");
+    });
+    socket.on("createGame", () => {
+        const roomUniqueId = makeid(6);
+        rooms[roomUniqueId] = {};
+        socket.join(roomUniqueId);
+        socket.emit("newGame", { roomUniqueId: roomUniqueId });
+      });
+      socket.on("joinGame", (data) => {
+        if (rooms[data.roomUniqueId] != null) {
+          socket.join(data.roomUniqueId);
+          socket.to(data.roomUniqueId).emit("playersConnected", {});
+          socket.emit("playersConnected");
+        }
+      });
+      
+  });
+  
+
+app.use(express.static(path.join(__dirname, '../public')));
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '../public/index.html');
+});
+
+app.get('/healthcheck', (req, res) => {
+    res.send('<h1>RPS App running...</h1>');
+});
+
+
+server.listen(PORT, () => {
+    console.log(`listening on http://localhost:${PORT}`);
+});
+function makeid(length) {
+    var result = "";
+    var characters =
+      "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    var charactersLength = characters.length;
+    for (var i = 0; i < length; i++) {
+      result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    return result;
+  }
+  
